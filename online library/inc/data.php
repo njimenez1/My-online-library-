04c909725134ea2e9f5d9cd65f95499e94b0a725
njimenez1 <45578615+njimenez1@users.noreply.github.com>
2018-12-05 19:45:26 -0500

    Add files via upload

---
+++ online library/inc/data.php
@@ -0,0 +1,164 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Burned",
+	"img" => "img/media/burned.jpg",
+    "genre" => "Young Adult",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+      "Ellen Hopkins"
+    ],
+		"publisher" => "Margaret K. McElderry",
+	  "isbn" => '9781416903550'
+];
+$catalog[102] = [
+    "title" => "Crank",
+    "img" => "img/media/crank.jpg",
+    "genre" => "Young Adult",
+    "format" => "Paperback",
+    "year" => 2004,
+    "category" => "Books",
+    "authors" => [
+        "Ellen Hopkins"
+    ],
+		"publisher" => "Simon & Schuster",
+  	"isbn" => '0689865198'
+];
+$catalog[103] = [
+    "title" => "Impulse",
+    "img" => "img/media/impulse.jpg",
+    "genre" => "Yound Adult",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Ellen Hopkins"
+    ],
+		"publisher" => "Simon & Schuster",
+  	"isbn" => '9781416903567'
+];
+$catalog[104] = [
+    "title" => "The Hunger Games",
+    "img" => "img/media/hun.jpg",
+    "genre" => "Adverture",
+    "format" => "Paperback",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Suzanne Collins"
+    ],
+		"publisher" => "Scholastic Press",
+  	"isbn" => '9780439023528'
+];
+//Movies
+$catalog[201] = [
+    "title" => "American Psycho",
+    "img" => "img/media/amer.jpeg",
+    "genre" => "Psychological Horror",
+    "format" => "DVD",
+    "year" => 2000,
+    "category" => "Movies",
+    "director" => "Mary Harron",
+    "writers" => [
+        "Bret Easton Ellis"
+    ],
+    "stars" => [
+			"Christian Bale",
+			"Jared Leto",
+			"Reese Witherspoon"
+    ]
+];
+$catalog[202] = [
+    "title" => "The Conjuring",
+    "img" => "img/media/con.jpg",
+    "genre" => "Horror",
+    "format" => "Blu-ray",
+    "year" => 2013,
+    "category" => "Movies",
+    "director" => "James Wan",
+    "writers" => [
+			"Chad Hayes",
+			"Carey W. Hayes"
+    ],
+    "stars" => [
+			"Vera Farmiga",
+			"Patrick Wilson",
+			"Lili Taylor",
+			"Ron Livingston"
+    ]
+];
+$catalog[203] = [
+    "title" => "Spirited Away",
+    "img" => "img/media/sp.jpg",
+    "genre" => "Animation/Fantasy",
+    "format" => "DVD",
+    "year" => 2002,
+    "category" => "Movies",
+    "director" => "Hayao Miyazaki",
+    "writers" => [
+        "Hayao Miyazaki"
+    ],
+    "stars" => [
+			"Daveigh Chase",
+			"Suzanna Pleshette",
+			"Jason Marsden"
+    ]
+];
+$catalog[204] = [
+    "title" => "Perfect blue",
+    "img" => "img/media/perf.jpg",
+    "genre" => "Animation/Psychological Horror",
+    "format" => "DVD",
+    "year" => 1999,
+    "category" => "Movies",
+    "director" => "Satoshi Kon",
+    "writers" => [
+			"Sadayuki Murai",
+			"Satoshi Kon"
+    ],
+    "stars" => [
+			"Junko Iwao",
+			"Rica Matsumoto",
+			"Shinpachi Tsuji"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Channel Orange",
+    "img" => "img/media/oran.jpg",
+    "genre" => "R&B",
+    "format" => "CD, Digital",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Frank Ocean"
+];
+$catalog[302] = [
+    "title" => "130 mood : TRBL",
+    "img" => "img/media/dean.jpg",
+    "genre" => "R&B",
+    "format" => "Digital",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Dean"
+];
+$catalog[303] = [
+    "title" => "Trilogy",
+    "img" => "img/media/tro.jpg",
+    "genre" => "R&B",
+    "format" => "Digital",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "The Weeknd"
+];
+$catalog[304] = [
+    "title" => "MOVE",
+    "img" => "img/media/move.jpg",
+    "genre" => "K-pop",
+    "format" => "CD, Digital",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Taemin"
+];
